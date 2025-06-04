1951fe08f5dd0de60cb04822256120134a42e8f1
heshamsyousef <al.hieder@gmail.com>
2025-06-04 10:05:44 -0400

    Update test.pl

--- test.pl
+++ test.pl
@@ -1,1 +1,13 @@
+#!/usr/bin/perl
+    use strict;
+    use warnings;
+	use File::stat;
 
+    my $directory = 'C:\Users\YousefH\Documents\gold';
+
+    opendir (DIR, $directory) or die $!;
+	while (my $file = readdir(DIR)) {
+           my $sb= system ( stat($file));
+        print "$sb->size:$file\n";
+
+    }
